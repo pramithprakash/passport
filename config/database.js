af95a9f53aece930e812e81b35d86365a2d38b57
Pramith Prakash <pramithprakash@gmail.com>
2015-06-29 14:24:39 +0530

    commititmg

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
-
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	url : 'mongodb://dbuser:dbpassword@ds047632.mongolab.com:47632/pramithdb'
+    //'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
